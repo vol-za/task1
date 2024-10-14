70e2e0b01bb4544b8421ccea911d18d0ee89d25b
vol-za <vol-za@mail.ru>
2024-10-14 21:19:32 +0300

    update 3.dart

--- oop/3.dart
+++ oop/3.dart
@@ -1,21 +1,46 @@
 abstract class Vehicle {
   void drive();
-  void stop() => print("${this.toString()} stopped");
+  void stop() => print('Stopped moving');
+  bool isSpeedLimitUp(double speed, double speedLimit) => speed > speedLimit;
 }
 
 class Car extends Vehicle {
+  double speed;
+  String color;
+  int _engineStatus;
+
+  Car(this.speed, this.color, this._engineStatus);
+
+  get getEngineStatus => _engineStatus;
+
+  set setEngineStatus(int status) => _engineStatus = status;
 
   @override
-  void drive() {
-    // TODO: implement drive
-  }
-
+  void drive() => print('Car is driving');
 }
 
 class Bicycle extends Vehicle {
+  double speed;
+  String color;
+  int engineStatus;
+
+  Bicycle(this.speed, this.color, this.engineStatus);
+
   @override
-  void drive() {
-    // TODO: implement drive
+  void drive() => print('Bicycle is driving');
+}
+
+void main() {
+  final car = Car(65, "black", 1);
+  if (!car.isSpeedLimitUp(car.speed, 60)) {
+    car.drive();
+    car.stop();
   }
 
+  
+  final bicycle = Bicycle(20, "black", 0);
+    if (!bicycle.isSpeedLimitUp(bicycle.speed, 60)) {
+    bicycle.drive();
+    bicycle.stop();
+  }
 }
